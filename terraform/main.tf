125a469e75d75b6188bb7605de96c297062551f9
Vitalii Tutkievych <vitalii.tutkievych@dataart.com>
2022-11-09 16:55:10 +0200

    [DBD-102] Create account sandbox

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,23 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "aws-sandbox@dbdventures.com"
+    AccountName               = "sandbox"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws-sandbox@dbdventures.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Owner"       = "DBD Ventures"
+    "Project"     = "DBD"
+    "Environment" = "sandbox"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "vitalii.tutkievych@dataart.com"
+    change_reason       = "Create sandbox account"
   }
 
   custom_fields = {
